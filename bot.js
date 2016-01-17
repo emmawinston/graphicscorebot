e67a444914aebb4a2e792c473bd0bbabb60d0ae2
Emma Winston <emma.winston@gmail.com>
2016-01-17 13:58:42 +0000

    initial commit

---
+++ bot.js
@@ -0,0 +1,12 @@
+{
+	"origin":["{svg <?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><svg xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"500\" height=\"500\" viewBox=\"0 0 200 200\" style=\"enable-background:new 0 0 200 200;\" id=\"svg2\" xml:space=\"preserve\">#line# #rect#</svg>}"],
+
+		"line":["<polyline points=\"#leastone#0,#leastone#0 #leastone#0,#leastone#5 #leastone#0,#leastone#0 #leastone#0,#leastone#20 #leastone#20,#leastone#40 #leastone#00,#leastone#80\" style=\"fill:none;stroke:\\##color#;stroke-width:#leastone#;stroke-linejoin:round\"    />"],
+	
+	"rect":["<rect x=\"#digit##digit#\" y=\"#digit##digit#\" height=\"#leastone##digit#\" width=\"#leastone##digit#\" style=\"fill: \\##color#\" opacity = \"#small##digit#\" #maybeBlur#/>"],
+	"color":["#hex##hex##hex##hex##hex##hex#"],
+	"hex":["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"],
+	"digit":["1","2","3","4","5","6","7","8","9", "0"],
+	"leastone":["1","2","3","4","5","6","7","8","9"],
+	"small":["0","1"]
+}
